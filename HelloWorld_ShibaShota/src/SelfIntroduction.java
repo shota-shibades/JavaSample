00c506e4425f3042dbbb892020dd2e92f1e58354
shota-shiba <syota.0802.2@gmail.com>
2023-06-06 09:42:44 +0900

    自己紹介の編集

--- HelloWorld_ShibaShota/src/SelfIntroduction.java
+++ HelloWorld_ShibaShota/src/SelfIntroduction.java
@@ -1,4 +1,21 @@
 
 public class SelfIntroduction {
 
+	public static void main(String[] args) {
+		// 氏名
+		String name = "芝 翔大";
+		// 年齢
+		int age = 22;
+		// 趣味
+		String hobby = "アニメ";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		System.out.println("僕の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("好きな事はは" + hobby + "です。");
+
+	}
+
 }
