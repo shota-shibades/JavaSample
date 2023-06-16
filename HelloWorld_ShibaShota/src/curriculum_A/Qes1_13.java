ade42a034496e4949260c3ce717d8ba9f0f773b9
shota-shiba <syota.0802.2@gmail.com>
2023-06-16 16:54:25 +0900

    課題1.8回答

---
+++ HelloWorld_ShibaShota/src/curriculum_A/Qes1_13.java
@@ -0,0 +1,170 @@
+package curriculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		
+		/* 1.)下記9個をローカル変数として宣言のみしてください
+		 *・バイト型・短整数型・整数型・長整数型
+		 *・単精度浮動小数点数型・倍精度浮動小数点数型
+		 *・文字型・文字列型
+		 *・ブーリアン型
+		 */
+		
+		// バイト型
+		byte b;
+		// 短整数型
+		short s;
+		// 整数型
+		int i;
+		// 長整数型
+		long l;
+		// 単精度浮動小数点数型
+		float f;
+		// 倍精度浮動小数点数型
+		double d;
+		// 文字型
+		char c;
+		// 文字列型
+		String S;
+		// ブーリアン型
+		boolean B;
+		
+		// 2.)それぞれのローカル変数をローカル内でそれぞれの初期値を代入し初期化してください
+		b = 0;
+		s = 0;
+		i = 0;
+		l = 0;
+		f = 0F;
+		d = 0;
+		c = 0;
+		S = null;
+		B = false;
+		
+		// 3.)初期化をしたそれぞれの変数に下記の値を代入してください
+		b = 10;
+		s = 100;
+		i = 1000;
+		l = 10000;
+		f = 9.5F;
+		d = 10.5;
+		c = 'a';
+		S = "ハロー";
+		B = true;
+
+		/* 4.)下記の通りにコンソール出力されるようにしてください
+		 *上記で作成した変数を必ず使用すること
+		 */
+		System.out.println(b + s + i + l);
+		
+		System.out.println(b * 2);
+		
+		System.out.println(c + S + B);
+		
+		int total = (int)(b + s + i + l + f + d);
+		System.out.println(total);
+		
+		System.out.println(b * s * i * l);
+		
+		System.out.println(d/s);
+		
+		System.out.println(b - s);
+		
+		// .5)プログラムを修正して、意図する処理に直す
+		String num="20";
+		int num1=23;
+		/* System.out.println("ハローJAVA"+(num+num1));の記述だと"20+23=43"
+		 * の計算が行われてしまうので、下記のように文字列の結合とすることで"2023"と出力できる
+		 */
+		System.out.println("ハローJAVA" + num + num1);
+		
+		/* .6)『』で囲われた人の情報を変数にして、formatの通りコンソールに出力してください
+		 * ローカル変数に代入し○○に入れてください
+		 * 『山田太郎 18歳 170.5cm 62.2kg 寿司』
+		 */
+		
+		//変数を定義
+		//名前
+		String name = "山田太郎";
+		//年齢
+		int age = 18;
+		//身長
+		double height = 170.5;
+		//体重
+		double weight = 62.2;
+		//食べ物
+		String favorite = "寿司";
+		//出力
+		System.out.printf("「初めまして%sです」%n",name);
+		System.out.printf("「年齢は%d歳です」%n",age);
+		System.out.printf("「身長は%.1fcmです」%n",height);
+		System.out.printf("「体重は%.1fkgです」%n",weight);
+		System.out.printf("「好きな食べ物は%sです」%n",favorite);
+		
+		// .7)BMIを出力
+		// 身長の単位を'm'に直す
+		double heightBmi = height*0.01;
+		// BMIの計算はBMI＝体重(kg) ÷ [身長(m) Ｘ 身長(m)
+		double bmi = weight/(heightBmi*heightBmi);
+		System.out.printf("「BMIは%fです」",bmi);
+		
+		// .8).6で宣言した変数に再代入し下記の通りコンソールに出力してください
+		// 再代入
+		name = "鈴木一郎";
+		age = 24;
+		height = 168.5;
+		weight = 64.2;
+		favorite = "オムレツ";
+		heightBmi = height*0.01;
+		bmi = weight/(heightBmi*heightBmi);
+		
+		// 出力
+		System.out.printf("初めまして%sです%n",name);
+		System.out.printf("年齢は%d歳です%n",age);
+		System.out.printf("身長は%.1fcmです%n",height);
+		System.out.printf("体重は%.1fkgです%n",weight);
+		System.out.printf("好きな食べ物は%sです%n",favorite);
+		System.out.printf("BMIは%.1fです",bmi);
+		
+		// .9).8で使用した変数を和算で自己代入し出力
+		
+		age +=age;
+		height +=height;
+		weight +=weight;
+		favorite +=favorite;
+		heightBmi = height*0.01;
+		bmi = weight/(heightBmi*heightBmi);
+		
+		System.out.printf("初めまして%sです%n",name);
+		System.out.printf("年齢は%d歳です%n",age);
+		System.out.printf("身長は%.1fcmです%n",height);
+		System.out.printf("体重は%.1fkgです%n",weight);
+		System.out.printf("好きな食べ物は%sです%n",favorite);
+		System.out.printf("BMIは%.2fです%n",bmi);
+		
+		// .10) 8で使用した年齢が25歳以上ならtrueが出力されるようにしてください。ただしif文は使いません
+		System.out.println(height >= 25);
+		
+		// .11) 8で使用した【年齢・身長・体重】を文字列型に型変換し繋げて出力してください
+		String ageString = String.valueOf(age);
+		String heightString = String.valueOf(height);
+		String weightString = String.valueOf(weight);
+		//StringBuilderで文字列を連結
+		StringBuilder Suzuki = new StringBuilder();
+		Suzuki.append(ageString);
+		Suzuki.append(heightString);
+		Suzuki.append(weightString);
+		System.out.println(Suzuki.toString());
+		// .12)11で変換した【年齢・身長】を整数型に変換して出力してください
+		int ageInt = Integer.parseInt(ageString);
+		int heightInt = (int)Double.parseDouble(heightString);
+		System.out.println(ageInt);
+		System.out.println(heightInt);
+		
+		// .13)12で変換した【年齢・身長】で【年齢が25もしくは身長が160以上】であればtrueを出力してください
+		//どちらか一つの条件に当てはまる場合なので論理和"||"
+		System.out.println(ageInt > 25 || heightInt > 160);
+
+	}
+
+}
