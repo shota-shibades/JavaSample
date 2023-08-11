1857846756f7deec5dc9e8bad3153b865e968905
shota-shiba <syota.0802.2@gmail.com>
2023-08-11 18:54:18 +0900

    8/11 クラスの基本、課題提出

--- HelloWorld_ShibaShota/src/basicClass/Animal.java
+++ HelloWorld_ShibaShota/src/basicClass/Animal.java
@@ -10,12 +10,17 @@ public class Animal {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		System.out.println(new Dog().animal);
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		//乱数を作成
 		Random rnd = new Random();
 		int value = rnd.nextInt(10);
+		//変数valueを引数に渡してDogクラスを呼び出す
 		System.out.println(new Dog(value).count);
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-		Date exDate = new Date();   // 2．日付（今回は現在の日時）を取得
-		SimpleDateFormat exDateFormat = new SimpleDateFormat("yyyy-MM-dd H:m:S"); // 3．パターンを指定
+		// 現在の日時を取得
+		Date exDate = new Date();
+		// 表示形式を指定
+		SimpleDateFormat exDateFormat = new SimpleDateFormat("yyyy-MM-dd H:m:S");
+		// 出力
 		System.out.println(exDateFormat.format(exDate));
 	}
 }
