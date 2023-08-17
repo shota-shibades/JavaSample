dc58d891744dfe5edf308d24dc09838bf3ab7200
shota-shiba <syota.0802.2@gmail.com>
2023-08-17 11:11:40 +0900

    8/17 課題1.20~22提出

--- Practice/src/execution/Test02.java
+++ Practice/src/execution/Test02.java
@@ -1,11 +1,15 @@
+// 実行用のパッケージ
 package execution;
 
 public class Test02{
 
 	public static void main(String[] args) {
 
+		// 処理パッケージで作成したクラスをインスタンス化
 		process.Test01 test = new process.Test01();
+		// setterメソッドで変数foodに寿司を代入
 		test.setValiable("寿司");
+		// 　出力メソッドで出力
 		test.show();
 	}
 
--- Practice/src/process/Test01.java
+++ Practice/src/process/Test01.java
@@ -1,3 +1,4 @@
+// 処理用のパッケージ
 package process;
 
 import java.text.SimpleDateFormat;
@@ -7,18 +8,25 @@ public class Test01 {
 	private String food;
 
 
+	// 出力するコンストラクタを定義
 	public void show() {
+		// 日付フォーマットをインスタンス化
 		Date exDate = new Date();
+		// 日付フォーマットを使って表示形式を指定
 		SimpleDateFormat exDateFormat = new SimpleDateFormat("yyyy/MM/dd H:m:s");
-		
+		// コンソールに出力
 		System.out.println("こんにちは！ここは日本です！");
+		// フィールドで定義した変数foodを指定して出力
 		System.out.println("この " + this.food + "はうまい");
 		System.out.println(this.food + "は和食です");
-		System.out.println("今の現在日時は" +exDateFormat.format(exDate)+ "です");
+		// exDateFormatで指定した形式で出力
+		System.out.println("今の現在日時は" + exDateFormat.format(exDate) + "です");
 	}
 	
+	// セッターメソッドで変数を定義
 	public void setValiable(String food) {
 		this.food = food;
+		// nullチェック(foodの中身がnullの場合"null"と出力)
 		if ( food == null ) {
 		    System.out.println("str == null");
 		}
