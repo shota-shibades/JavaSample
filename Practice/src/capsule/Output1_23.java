b6ff25c19bb2f131e6b49fbf6bbb882c55a596c5
shota-shiba <syota.0802.2@gmail.com>
2023-08-17 15:07:59 +0900

    8/17 課題1.23提出(02

--- Practice/src/capsule/Output1_23.java
+++ Practice/src/capsule/Output1_23.java
@@ -3,10 +3,8 @@ package capsule;
 public class Output1_23 {
 	public static void main(String[] args) {
 		// 処理用のクラスをインスタンス化
-		Qes1_23 capsule = new Qes1_23();
+		Qes1_23 capsule = new Qes1_23("ライオン",2.1,80);
 
-		// setterメソッドでそれぞれの変数を定義
-		capsule.setNameLengthSpeed("ライオン", 2.1, 80);
 		// getterメソッドで変数を取得し出力
 		System.out.println("  動物名:" + capsule.getName());
 		System.out.println("体長:" + capsule.getLength() + "m");
