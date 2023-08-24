a745bb7ac5faa1a4b86dd032a7911845dc61f29a
shota-shiba <syota.0802.2@gmail.com>
2023-08-24 10:40:07 +0900

    8/24 課題1_26~28 提出(02

--- Practice/src/Qes26_28/Animals.java
+++ Practice/src/Qes26_28/Animals.java
@@ -2,52 +2,117 @@ package Qes26_28;
 
 public class Animals {
 
-	// それぞれの動物の情報を格納
+	String name;
+	double length;
+	int speed;
+	String logicalName;
+
+	public void get(String id[]) {
+		// 受け取った配列の要素数だけ繰り返すfor文
+		for (int i = 0; i < id.length; i++) {
+			switch (id[i]) {
+			// 下記の値を受け取った場合対応する動物の情報をそれぞれの変数に代入しprintInfo()で出力
+			case "ライオン:2.1:80": {
+				this.name = lion.name;
+				this.length = lion.length;
+				this.speed = lion.speed;
+				this.logicalName = lion.logicalName;
+				printInfo();
+				break;
+			}
+			case "ゾウ:3.2:40": {
+				this.name = elephant.name;
+				this.length = elephant.length;
+				this.speed = elephant.speed;
+				this.logicalName = elephant.logicalName;
+				printInfo();
+				break;
+			}
+			case "パンダ:1.9:30": {
+				this.name = panda.name;
+				this.length = panda.length;
+				this.speed = panda.speed;
+				this.logicalName = panda.logicalName;
+				printInfo();
+				break;
+			}
+			case "チンパンジー:0.94:25": {
+				this.name = chimpanzee.name;
+				this.length = chimpanzee.length;
+				this.speed = chimpanzee.speed;
+				this.logicalName = chimpanzee.logicalName;
+				printInfo();
+				break;
+			}
+			case "シマウマ:2.4:65": {
+				this.name = zebla.name;
+				this.length = zebla.length;
+				this.speed = zebla.speed;
+				this.logicalName = zebla.logicalName;
+				printInfo();
+				break;
+			}
+			case "インコ:0.1:50": {
+				this.name = parakeet.name;
+				this.length = parakeet.length;
+				this.speed = parakeet.speed;
+				this.logicalName = parakeet.logicalName;
+				printInfo();
+				break;
+			}
+			// 当てはまらない値を受け取った場合の出力
+			default:
+				System.out.println("不明");
+			}
+		}
+	}
+
+	// 出力用メソッド
+	public void printInfo() {
+		System.out.println("	動物名：" + this.name + "\n 体長：" + this.length + "m" + "\n 速度：" + this.speed + "km/h"
+				+ "\n 学名：" + this.logicalName);
+	}
+
+	// それぞれの動物の情報を格納　変数idはmainクラスで分岐するときに使用
 	class lion {
-		public String name = "ライオン";
-		public double length = 2.1;
-		public int speed = 80; 
-		public String logicalName = "パンテラ レオ";
-		static String id = "2.1:80";
+		static String name = "ライオン";
+		static double length = 2.1;
+		static int speed = 80;
+		static String logicalName = "パンテラ レオ";
 	}
-	
-	class elephant{
-		public String name = "ゾウ";
-		public double length = 3.2;
-		public int speed = 40;
-		public String logicalName = "ロキソドンタ・サイクロティス";
-		static String id = "3.2:40";
+
+	class elephant {
+		static String name = "ゾウ";
+		static double length = 3.2;
+		static int speed = 40;
+		static String logicalName = "ロキソドンタ・サイクロティス";
 	}
-	
-	class panda{
-		public String name = "パンダ";
-		public double length = 1.9;
-		public int speed = 30;
-		public String logicalName = "アイルロポダ・メラノレウ";
-		static String id = "1.9:30";
+
+	class panda {
+		static String name = "パンダ";
+		static double length = 1.9;
+		static int speed = 30;
+		static String logicalName = "アイルロポダ・メラノレウ";
 	}
-	
-	class chimpanzee{
-		public String name = "チンパンジー";
-		public double length = 0.94;
-		public int speed = 25;
-		public String logicalName = "パン・トゥログロディテス";
-		static String id = "0.94:2";
+
+	class chimpanzee {
+		static String name = "チンパンジー";
+		static double length = 0.94;
+		static int speed = 25;
+		static String logicalName = "パン・トゥログロディテス";
 	}
-	
-	class zebla{
-		public String name = "シマウマ";
-		public double length = 2.4;
-		public int speed = 65;
-		public String logicalName = "チャップマンシマウマ";
-		static String id = "2.4:65";
+
+	class zebla {
+		static String name = "シマウマ";
+		static double length = 2.4;
+		static int speed = 65;
+		static String logicalName = "チャップマンシマウマ";
 	}
-	
-	class parakeet{
-		public String name = "インコ";
-		public double length = 0.1;
-		public int speed = 50;
-		public String logicalName = "不明";
-		static String id = "0.1:50";
+
+	class parakeet {
+		static String name = "インコ";
+		static double length = 0.1;
+		static int speed = 50;
+		static String logicalName = "不明";
 	}
 }
--- Practice/src/Qes26_28/main.java
+++ Practice/src/Qes26_28/main.java
@@ -2,32 +2,22 @@ package Qes26_28;
 
 import java.util.Scanner;
 
-import Qes26_28.Animals.chimpanzee;
-import Qes26_28.Animals.elephant;
-import Qes26_28.Animals.lion;
-import Qes26_28.Animals.panda;
-import Qes26_28.Animals.parakeet;
-import Qes26_28.Animals.zebla;
-
 public class main {
 
 	public static void main(String[] args) {
 		// scannerクラスを用意
 		Scanner sc = new Scanner(System.in);
-		Animals_sub As = new Animals_sub();
+		//Animals_sub As = new Animals_sub();
 		String order = "";
 		// コンソールに入力を要求
 		System.out.println("コンソールに文字を入力してください");
 		// コンソールの入力を変数orderに格納
 		order = sc.nextLine();
 		sc.close();
+		// 受け取った変数を","で区切って配列に格納
+		String orders[] = order.split(",");
 		// 入力された情報を引数にメソッドを呼び出す
-		if (order.equals(lion.id) || order.equals(elephant.id) || order.equals(panda.id) || order.equals(chimpanzee.id)
-				|| order.equals(zebla.id) || order.equals(parakeet.id))
-		{
-			As.output(order);
-		}else{
-			System.out.println("情報がありません");
-		}
+		Animals a = new Animals();
+		a.get(orders);
 	}
 }
