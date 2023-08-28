fc913813d8c0b4a18806c143c2181f48d4a35ca2
shota-shiba <syota.0802.2@gmail.com>
2023-08-28 11:00:35 +0900

    8/28 課題1_26~28 提出(04

--- Practice/src/Qes26_28/Animals.java
+++ Practice/src/Qes26_28/Animals.java
@@ -2,16 +2,18 @@ package Qes26_28;
 
 public class Animals {
 
-	String name;
-	double length;
-	int speed;
-	String logicalName;
+	// 他のクラスから直接変更ができないようにアクセス修飾子private
+	private String name;
+	private double length;
+	private int speed;
+	private String logicalName;
 
-	String getlogicalName(String name) {
+	// 名前から学名を取得できるコンストラクタ
+	public String getlogicalName(String name) {
 		switch (name) {
 		case "ライオン":
 			return "パンテラ レオ";
-		case "ゾウ:":
+		case "ゾウ":
 			return "ロキソドンタ・サイクロティス";
 		case "パンダ":
 			return "アイルロポダ・メラノレウ";
@@ -24,6 +26,7 @@ public class Animals {
 		}
 	}
 
+	// 取得した学名とコンソールから取得した変数を代入
 	public Animals(String name, double length, int speed) {
 		this.name = name;
 		this.length = length;
@@ -34,7 +37,7 @@ public class Animals {
 
 	// 出力用メソッド
 	public void printInfo() {
-		System.out.println("	動物名：" + this.name + "\n 体長：" + this.length + "m" + "\n 速度：" + this.speed + "km/h"
+		System.out.println("動物名：" + this.name + "\n 体長：" + this.length + "m" + "\n 速度：" + this.speed + "km/h"
 				+ "\n 学名：" + this.logicalName);
 	}
 }
--- Practice/src/Qes26_28/execution.java
+++ Practice/src/Qes26_28/execution.java
@@ -7,24 +7,23 @@ public class execution {
 	public static void main(String[] args) {
 		// scannerクラスを用意
 		Scanner sc = new Scanner(System.in);
-		//Animals_sub As = new Animals_sub();
 		String order = "";
 		// コンソールに入力を要求
 		System.out.println("コンソールに文字を入力してください");
-		// コンソールの入力を変数orderに格納
+		// コンソールの入力を変数に格納
 		order = sc.nextLine();
 		sc.close();
-		// 受け取った変数を","で区切って配列ordersに格納
+		// 受け取った変数を","で区切って配列に格納
 		String orders[] = order.split(",");
-		// 配列ordersを":"で区切り配列Animalに格納
+		// 配列を":"で区切り配列に格納
 		for (int i = 0; i < orders.length; i++) {
-			String Animal[] = orders[i].split(":");
-			// 区切った１つ目の要素を変数nameに格納
-			String name = Animal[0];
-			// 区切った２つ目の要素をdouble型に変換し変数speedに格納 
-			double length = Double.parseDouble(Animal[1]);
-			// 区切った3つ目の要素をint型に変換し変数speedに格納 
-			int speed = Integer.parseInt(Animal[2]);
+			String animals[] = orders[i].split(":");
+			// 区切った１つ目の要素を変数に格納
+			String name = animals[0];
+			// 区切った２つ目の要素をdouble型に変換し変数に格納 
+			double length = Double.parseDouble(animals[1]);
+			// 区切った3つ目の要素をint型に変換し変数に格納 
+			int speed = Integer.parseInt(animals[2]);
 			// コンストラクタでそれぞれを格納し出力
 			Animals a = new Animals(name, length, speed);
 			a.printInfo();
